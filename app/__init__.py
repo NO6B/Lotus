5fba34aa5e5f3dd550200fe3d00508b72a9f92e7
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-03-27 23:44:43 +0100

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 from config import Config
 
-
 db = SQLAlchemy()
+mail = Mail()
 
 
 def my_app():
-	
-	app = Flask(__name__, 
-                template_folder='front', 
+
+    app = Flask(__name__,
+                template_folder='front',
                 static_folder='front')
-	app.config.from_object(Config)
+    app.config.from_object(Config)
 
-	# Initialize extensions
-	from app.routes import bp
-	app.register_blueprint(bp)
-	db.init_app(app)
+    from app.routes import bp
+    app.register_blueprint(bp)
+    db.init_app(app)
+    mail.init_app(app)
 
-	return app
+    return app
